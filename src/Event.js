d4935e22fd5eff4d60bc2a074ac897339290c611
Muhsin <paravathmuhsin@gmail.com>
2022-10-11 08:26:09 +0530

    event handling

--- src/Event.js
+++ src/Event.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 export default class Event extends Component {
-  clickHandler = () => {
-    console.log('sdsd')
+  constructor() {
+    super();
+    // this.bindedClickHandler = this.clickHandler.bind(this);
+  }
+  state = {
+    name: "Teenu",
+  };
+  clickHandler = (e, target) => {
+    console.log(e, target);
   };
   render() {
-    return <div onClick={this.clickHandler}>Event</div>;
+    return (
+      <>
+        <div
+          onClick={(e) => {
+            this.clickHandler(e, "Event 1");
+          }}
+        >
+          Event 1
+        </div>
+        <div onClick={(e) => {
+            this.clickHandler(e, "Event 2");
+          }}>Event 2</div>
+      </>
+    );
   }
 }
