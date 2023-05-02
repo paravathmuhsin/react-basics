61fae480b883cec74dde4a66fa2c0d3e6925aef4
Muhsin <paravathmuhsin@gmail.com>
2023-05-02 10:50:33 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  render() {
+    return <h1 className="App">Hello</h1>;
+  }
+}
+
+export default App;
