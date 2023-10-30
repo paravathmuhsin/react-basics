f11647c9cda49efac6f9968f6e9736d371191946
Thasnim P <thasnimprvt@gmail.com>
2023-10-30 09:12:02 +0530

    s

--- src/Context.js
+++ src/Context.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useThemeContext } from "./App";
 
 const Context = () => {
+    // useContext(ThemeContext);
   const context = useThemeContext();
   return <div>Context - {context}</div>;
 };
