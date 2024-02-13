602cb15f4d0ddb138805d048ff07244898529c01
Muhsin <paravathmuhsin@gmail.com>
2024-02-13 09:05:59 +0530

    class comp

--- src/ClassComp.js
+++ src/ClassComp.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 
 class ClassComp extends Component {
+  constructor(){
+    super();
+    console.log('constructor');
+  }
   // state managment
+  state = {
+    age: 44,
+    counter: 0,
+    country: "IN",
+  };
   name = "David";
   clickHandler = () => {
-    console.log("ss");
     this.name = "John";
+    // async
+    this.setState({ ...this.state, age: 66, country: 'SL' });
   };
   buttonClickHander = () => {
-    console.log(this.name);
+    // this.setState({...this.state, counter: this.state.counter + 1})
+    this.setState((state) => {
+      return { ...state, counter: state.counter + 1 };
+    });
   };
+
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate')
+  }
+  shouldComponentUpdate(props, state){
+    console.log('shouldComponentUpdate', state, this.state)
+    return state.age !== this.state.age;
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+  }
   render() {
+    console.log('render')
     return (
       <div>
-        <div onClick={this.clickHandler}>ClassComp - {this.name}</div>
-        <button onClick={this.buttonClickHander}>Click me</button>
+        <div onClick={this.clickHandler}>
+          ClassComp - {this.name}@{this.state.age} - {this.state.country}
+        </div>
+        <button onClick={this.buttonClickHander}>
+          Click me - {this.state.counter}
+        </button>
       </div>
     );
   }
