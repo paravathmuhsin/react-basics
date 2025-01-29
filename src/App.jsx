38fc70dbfc7ba31791bb61bdcafb2ef92e45e4d4
Muhsin <paravathmuhsin@gmail.com>
2025-01-29 09:08:56 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+  return <h2>Welcome to my first React app</h2>;
 }
 
-export default App
+export default App;
