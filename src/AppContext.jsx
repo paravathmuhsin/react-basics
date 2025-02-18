42abbe687e77ffcc62aaf37bf71db3861906f6a9
Muhsin <paravathmuhsin@gmail.com>
2025-02-18 07:40:52 +0530

    context update for v19

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,13 +1,16 @@
+import { use } from "react";
 import { createContext, useContext, useState } from "react";
 
 const Context = createContext("light");
 
-export const useAppContext = () => useContext(Context);
+// export const useAppContext = () => useContext(Context);
+export const useAppContext = () => use(Context);
 
 const AppContext = ({ children }) => {
   const [theme, setTheme] = useState("light");
   return (
-    <Context.Provider value={{ theme, setTheme }}>{children}</Context.Provider>
+    // <Context.Provider value={{ theme, setTheme }}>{children}</Context.Provider>
+    <Context value={{ theme, setTheme }}>{children}</Context>
   );
 };
 
